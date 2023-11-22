de923ec5d9f8e71f6e3ecc15f481ecfcf9effe79
Michel Guerra Defranchi <guerra.defranchi@protonmail.com>
2023-11-22 22:31:58 +0100

    FIX: fixed result mismatches

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 
 use std::io;
+use std::process;
 use rand::seq::SliceRandom;
 
 fn main() {
 
     let base_colors: [&str; 6] = ["Red", "Green", "Yellow", "Blue", "Purple", "Orange"];
-    let mut colors_to_match: [&str; 4] = Default::default();
-    let mut user_guess = Vec::new();
     let code_length: i32 = 4;
     let max_attemps: i32 = 10;
-    let mut attemps: i32 = 0;
+    let mut colors_to_match: [&str; 4] = Default::default();
+    let mut user_guess = Vec::new();
+    let mut attempts: i32 = 0;
     let mut guess = String::new();
     let mut correct_position: i32 = 0;
     let mut correct_color: i32 = 0;
@@ -27,12 +28,15 @@ fn main() {
         }
     }
 
-    println!("{:?}", colors_to_match);
+    // println!("{:?}", colors_to_match);
 
     println!("\nWelcome to the Matermind Game!!\n\nAvailable colors: {}\nCode length: {}, Max attemps: {}", base_colors.join(", "), code_length, max_attemps);
 
 
-    while attemps < max_attemps{
+    while attempts < max_attemps{
+
+        println!("Attempt: {}/{}. Enter yrou guess.", (attempts + 1), max_attemps);
+
         let _ = &mut user_guess.clear();
         let _ = &mut guess.clear();
         correct_color = 0;
@@ -62,16 +66,29 @@ fn main() {
             if i.0 == i.1 {
                 correct_position += 1;
             }
-            // This does not work
-            if colors_to_match.contains(i.1) {
-                correct_color += 1;
-            }
         }
 
+        // Need improvement
+        for i in colors_to_match {
+            if user_guess.contains(&i) {
+                correct_color += 1;
+            }
 
-        println!("{:?}, correct pos {}, correct color {}", user_guess, correct_position, correct_color);
+        }
+
+        correct_color -= correct_position; 
+
+        if correct_position == colors_to_match.len() as i32 {
+            println!("Congratulations!! You win!");
+            process::exit(0)
+        } 
 
 
-        attemps += 1;
+        println!("{} colors placed correctly!\n{} correct colors placed in the wrong position!", correct_position, correct_color);
+
+
+        attempts += 1;
     }
+    println!("No more attempts, You lose!");
+    println!("{}", colors_to_match.join(", "));
 }
