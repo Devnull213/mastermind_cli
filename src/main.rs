58c0640ba0f0a62c952160cb2347d265af8b9ca7
Michel Guerra Defranchi <guerra.defranchi@protonmail.com>
2023-11-23 18:57:50 +0100

    MOD: changes in prompt strings & changes in for loops

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-
+use rand::seq::SliceRandom;
 use std::io;
 use std::process;
-use rand::seq::SliceRandom;
 
 fn main() {
-
     let base_colors: [&str; 6] = ["Red", "Green", "Yellow", "Blue", "Purple", "Orange"];
     let code_length: i32 = 4;
     let max_attemps: i32 = 10;
     let mut colors_to_match: [&str; 4] = Default::default();
-    let mut user_guess = Vec::new();
     let mut attempts: i32 = 0;
-    let mut guess = String::new();
-    let mut correct_position: i32 = 0;
-    let mut correct_color: i32 = 0;
 
-
-    for index in 0..4{
-
+    // Creating a random array witn colors
+    for index in 0..code_length as usize {
         let mut rng = rand::thread_rng();
-
         let random_element = base_colors.choose(&mut rng);
 
         match random_element {
@@ -28,67 +20,55 @@ fn main() {
         }
     }
 
-    // println!("{:?}", colors_to_match);
-
     println!("\nWelcome to the Matermind Game!!\n\nAvailable colors: {}\nCode length: {}, Max attemps: {}", base_colors.join(", "), code_length, max_attemps);
 
+    while attempts < max_attemps {
+        let mut guess = String::new();
 
-    while attempts < max_attemps{
+        println!(
+            "Attempt: {}/{}. Enter yrou guess.",
+            (attempts + 1),
+            max_attemps
+        );
 
-        println!("Attempt: {}/{}. Enter yrou guess.", (attempts + 1), max_attemps);
-
-        let _ = &mut user_guess.clear();
-        let _ = &mut guess.clear();
-        correct_color = 0;
-        correct_position = 0;
-
-
+        // Processing user input
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        user_guess = guess.trim()
-            .split(" ")
-            .collect();
+        let user_guess: Vec<_> = guess.trim().split(" ").collect();
 
-        if !(user_guess.iter().all(|&item| base_colors.contains(&item)) && user_guess.len() == colors_to_match.len()) {
+        // Validating length of the user guess and contents
+        if !(user_guess.iter().all(|&item| base_colors.contains(&item))
+            && user_guess.len() == colors_to_match.len())
+        {
             println!("Invalid Guess, make sure you have exactly four colors!");
             continue;
         }
 
-        let zipped: Vec<_> = colors_to_match.iter()
-            .zip(user_guess
-            .iter())
-            .collect();
+        let zipped: Vec<_> = colors_to_match.iter().zip(user_guess.iter()).collect();
 
-        // Need improvement
-        for i in zipped{
-            if i.0 == i.1 {
-                correct_position += 1;
-            }
-        }
+        let correct_position: i32 = zipped.iter().filter(|&(a, b)| a == b).count() as i32;
 
-        // Need improvement
-        for i in colors_to_match {
-            if user_guess.contains(&i) {
-                correct_color += 1;
-            }
+        let mut correct_color: i32 = colors_to_match
+            .iter()
+            .filter(|x| user_guess.contains(x))
+            .count() as i32;
 
-        }
-
-        correct_color -= correct_position; 
+        correct_color -= correct_position;
 
         if correct_position == colors_to_match.len() as i32 {
-            println!("Congratulations!! You win!");
+            println!("\n\nCongratulations!! You win!\nCode was: {}", colors_to_match.join(", "));
             process::exit(0)
-        } 
+        }
 
-
-        println!("{} colors placed correctly!\n{} correct colors placed in the wrong position!", correct_position, correct_color);
-
+        println!(
+            "\n{} colors placed correctly!\n{} correct colors placed in the wrong position!\n\n",
+            correct_position, correct_color
+        );
 
         attempts += 1;
     }
-    println!("No more attempts, You lose!");
+    println!("\n\nNo more attempts, You lose!");
     println!("{}", colors_to_match.join(", "));
 }
